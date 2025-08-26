9f2fb8e4d0f2ae0d8b5542d1378911687d60b0f7
Graylen <delilhabigelow@gmail.com>
2025-08-25 22:19:51 -0400

    Home page UI.

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,62 @@
-import { Text, View } from "react-native";
- 
+import { CartButton } from "@/components/cart-button";
+import { images, offers } from "@/constants";
+import cn from 'clsx';
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-bold text-blue-500 text-center font-quicksand-bold">
-        Welcome to React Native App!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: `#ffffff22` }}
+              >
+                {({ pressed }) => (
+                  <>
+                    <View className={`h-full w-1/2`}>
+                      <Image source={item.image} className={`size-full`} resizeMode={`contain`} />
+                    </View>
+
+                    <View className={cn(`offer-card__info`, isEven ? `pl-10` : `pr-10`)}>
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
+                    </View>
+                  </>
+                )}
+              </Pressable>
+            </View>
+          )
+        }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 ">
+            <View className="flex-start">
+              <Text className="small-bold text-primary uppercase">deliver to</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Morganton</Text>
+                <Image source={images.arrowDown} className="size-3" resizeMode="contain" />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
